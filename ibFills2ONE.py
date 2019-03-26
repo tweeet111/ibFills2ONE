08b1a53601e1cd8274078ea41e09eeb648dbb831
tweeet111 <tweeet@freenet.de>
2019-03-26 11:13:41 +0100

    minor changes in comments and for readability

--- ibFills2ONE.py
+++ ibFills2ONE.py
@@ -9,8 +9,6 @@ from dateutil import tz
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
 										 level=logging.INFO)
-#util.patchAsyncio()
-
 
 
 def import_fills():
@@ -23,7 +21,7 @@ def import_fills():
 		while ib.isConnected() == False and i<20:
 			try:
 				print("connecting...")
-				ib.connect('127.0.0.1', 7496, clientId=client, timeout=None)#tws 7496 gateway 4001
+				ib.connect('127.0.0.1', 7496, clientId=client, timeout=None) #tws 7496 gateway 4001 (see TWS settings)
 				ib.sleep(1)
 			except ConnectionRefusedError:
 				ib.sleep(3)
@@ -40,7 +38,7 @@ def import_fills():
 	ib.sleep(0.1)
 	conn_tws(0)
 	ib.sleep(0.1)
-	# call ib.fills() and get all recorded fills (usually last 7days ??)
+# call ib.fills() and get all recorded fills (usually last 7days)
 
 	fills = ib.fills()
 	ib.sleep(0.1)
@@ -50,7 +48,7 @@ def import_fills():
 	to_chicago = tz.gettz('America/Chicago')
 	t_str = dt.today().strftime('%Y%m%d')
 
-		# creating dataframe from fills list
+# creating dataframe from fills list
 	cols =["TradeDate" , "TradeTimeUTC" , "TradeTimeCDT", "TradeTime", "Buy/Sell" , "conID", "AssetClass" , "Multiplier", "Symbol" , "Quantity" , "TradePrice" , "IBCommission" , "Currency", "UnderlyingSymbol", "Right", "permId", "execId", "orderRef","Expiration", "Description"]
 	fills_df = pd.DataFrame([[dt.strftime(fill.execution.time.astimezone(to_local),'%Y%m%d'),
 													dt.strftime(fill.execution.time,'%H%M%S'),
@@ -74,28 +72,29 @@ def import_fills():
 													"IB Import"]
 												 for fill in fills if fill.contract.secType not in ['BAG','CASH']], columns=cols)
 
-	#disconnect from TWS
+#disconnect from TWS
 	ib.disconnect()
 	ib.waitOnUpdate(timeout=0.1)
 
-	# for stocks multiplier is 1 but is  provided by IB as ""
+# for stocks multiplier is 1 but is  provided by IB as ""
 	fills_df['Multiplier'] = fills_df['Multiplier'].replace("",1).astype(float)
-	# Add PnL manually beacause TWS reports inconsistantly
+# Add PnL manually beacause TWS reports inconsistantly
 	fills_df['NetCash'] = -(fills_df['Quantity']*fills_df['Multiplier']*fills_df['TradePrice'])-fills_df['IBCommission'].where(fills_df['Buy/Sell'] == "BOT") 
 	fills_df['NetCash'] = fills_df['NetCash'].fillna(
                        (fills_df['Quantity']*fills_df['Multiplier']*fills_df['TradePrice'])-fills_df['IBCommission'])
 
-	#######################################################################################################
-	# ONE import file with FlexQueryImport formatted headers
-	# filter for Option Trades only and SPX UL ( European options e.g. Covered Calls throw error in ONE)
+#######################################################################################################
+# ONE import file with FlexQueryImport formatted headers
+# filter for Option Trades only and UL by filter_list ( European options e.g. Covered Calls throw error in ONE)
 	filter_list = ['SPX', 'SPY']
 	df_one = fills_df.query('AssetClass == "OPT" & UnderlyingSymbol in @filter_list')
-	# filter for today's trades only if needed to keep data short and clear, assuming one usually imports during the same session
-	#df_one = df_one.query('TradeDate == @t_str')
+
+# filter for today's trades only if needed to keep data short and clear, assuming one usually imports during the same session
+# df_one = df_one.query('TradeDate == @t_str')
 
 	df_one = df_one[['TradeDate','TradeTime','Buy/Sell','AssetClass','Symbol','Quantity','TradePrice','IBCommission','UnderlyingSymbol','NetCash']].replace("BOT","BUY").replace("SLD","SELL")   # replace signal words "buy/sell" from ib.fills to import format
 
-	# create filename with date/time suffix
+# create filename with date/time suffix
 	t = dt.now().strftime('%Y%m%d-%H%M%S')
 	filename= "ONE_import_"+t+".csv"
 
@@ -104,12 +103,12 @@ def import_fills():
 		df_one.to_csv(filename, index=False)
 
 
-	#############################################################################################################
-	# Make Quantity values negative for Sells and positive for Buys ---- easier to calculate positions from tradelog.csv
+#############################################################################################################
+# Make Quantity values negative for Sells and positive for Buys ---- easier to calculate positions from tradelog.csv
 	
 	fills_df['Quantity'] = np.where(fills_df['Buy/Sell'] =='SLD', fills_df['Quantity']*(-1), fills_df['Quantity'])
 
-	############################################################################################################
+############################################################################################################
 
 	df_log = pd.read_csv("trade_log.csv")
 	df = pd.concat([df_log, fills_df])
